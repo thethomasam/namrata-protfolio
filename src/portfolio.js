21bafdee8239d256d49b5cd60c86d6fdc9ec675b
Sam Koshy Thomas <98753007+thethomasam@users.noreply.github.com>
2024-02-20 16:37:20 +1030

    dix deploy gitpages

--- src/portfolio.js
+++ src/portfolio.js
@@ -315,16 +315,16 @@ const podcastSection = {
 const contactInfo = {
     title: emoji("Contact Me ☎️"),
     subtitle:
-        "Discuss a project or just want to say hi? My Inbox is open for all.",
-    number: "+92-0000000000",
-    email_address: "saadpasta70@gmail.com"
+        "",
+    number: "",
+    email_address: "namrata.bhattacharya03@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
     userName: "twitter", //Replace "twitter" with your twitter username without @
-    display: true // Set true to display this section, defaults to false
+    display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
